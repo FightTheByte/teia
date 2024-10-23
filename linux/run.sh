ca10ebfa3b24e5ff105122e06ba3dd0516366a8a
Martin Cameron <mcameron.brix@gmail.com>
2024-10-23 18:53:17 +0100

    fixed conda naming error

--- linux/run.sh
+++ linux/run.sh
@@ -3,5 +3,5 @@
 cd /home/fightthebyte/teia/linux 
 docker run -d --rm -it --env ACCEPT_LICENSE=true --publish 1080:1080 tts-standalone 
 source /home/fightthebyte/miniconda3/etc/profile.d/conda.sh
-conda activate theia 
+conda activate teia 
 python3 ./run.py
